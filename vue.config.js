b0cdad022c49d8fd5676f2582aab176e1e4cf375
nikki <1047399527@qq.com>
2021-10-30 12:43:03 +0800

    修改接口

--- vue.config.js
+++ vue.config.js
@@ -39,10 +39,10 @@ module.exports = {
   // 跨域配置
   // module.exports = {
   devServer: { //记住，别写错了devServer//设置本地默认端口  选填
-    port: 8080,
+    port: 6666,
     proxy: { //设置代理，必须填
       '/api': { //设拦截器  拦截器格式   斜杠+拦截器名字，名字可以自己定
-        target: 'http://localhost:8082', //代理的目标地址
+        target: 'http://souldays.top:8082', //代理的目标地址
         changeOrigin: true, //是否设置同源，输入是的
         pathRewrite: { //路径重写
           '/api': '' //选择忽略拦截器里面的单词
